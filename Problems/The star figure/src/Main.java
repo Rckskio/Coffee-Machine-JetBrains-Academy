bab34581a42b8419904bf38260eb119d99ca723f
Luiz <l.henrique92@gmail.com>
2020-07-12 00:00:53 +0800

    Added resolution Star figure problem

--- Problems/The star figure/src/Main.java
+++ Problems/The star figure/src/Main.java
@@ -5,9 +5,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        String matrix[][] = new String[number][number];
-        String current = ". ";
-
+        String[][] matrix = new String[number][number];
         /*
         // Fill each row with ". " without quotes
         for (int i = 0; i < matrix.length; i++) {
@@ -16,13 +14,23 @@ class Main {
             }
         }
         */
-
-        // Fill each row with ". "
+        // My solution
+        // Fill each row with ". " without quotes
         for (String[] row: matrix) {
             Arrays.fill(row, ". ");
         }
 
+        int middleRow = number / 2;
+        int middleColumn = number / 2;
+        int count = matrix.length - 1;
 
+        // Fill the middle row and the middle column with "* " without quotes
+        for (int i = 0; i < matrix.length; i++, count--) {
+            matrix[i][i] = "* ";
+            matrix[middleRow][i] = "* ";
+            matrix[i][middleColumn] = "* ";
+            matrix[i][count] = "* ";
+        }
 
         // this code prints all 2-dimensional arrays
         for (String[] text : matrix) {
@@ -31,5 +39,22 @@ class Main {
             }
             System.out.println();
         }
+
+        //Better solution
+        /*
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][i] = '*';
+            matrix[i][matrix.length - 1 - i] = '*';
+            matrix[i][matrix.length / 2] = '*';
+            matrix[matrix.length / 2][i] = '*';
+        }
+
+        for (String[] text : matrix) {
+            for (String vector : text) {
+                System.out.print(vector);
+            }
+            System.out.println();
+        }
+         */
     }
 }
