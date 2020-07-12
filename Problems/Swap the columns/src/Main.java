83e59f0537f619708724ace9354f0228157a6a50
Luiz <l.henrique92@gmail.com>
2020-07-12 20:06:03 +0800

    Added proble 'Swap the columns' solved

--- Problems/Swap the columns/src/Main.java
+++ Problems/Swap the columns/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int c = matrix[i][a];
+            matrix[i][a] = matrix[i][b];
+            matrix[i][b] = c;
+        }
+        // First time resolution - excess loop for!
+        /*
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int c = matrix[i][a];
+                matrix[i][a] = matrix[i][b];
+                matrix[i][b] = c;
+                break;
+            }
+        }
+         */
+        for (int[] num : matrix) {
+            for (int print : num) {
+                System.out.print(print + " ");
+            }
+            System.out.println();
+        }
     }
 }
